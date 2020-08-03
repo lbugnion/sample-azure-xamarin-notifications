10d4ac29ab6b230410fef6e19a94e044a52d3c2c
lbugnion <laurent@galasoft.ch>
2020-08-03 10:36:38 +0200

    Cleanup

--- source/LearnTvNotif/LearnTvNotif/Model/Constants.cs
+++ source/LearnTvNotif/LearnTvNotif/Model/Constants.cs
@@ -2,8 +2,8 @@
 {
     public static class Constants
     {
-        public const string HubName = "LbNotif";
-        public const string HubConnectionString = "Endpoint=sb://lbnotif.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=1uJue0a2XdOlhMSBMIjQJ1KSlojDWgXG7/kSUvB1SiA=";
+        public const string HubName = "{HUB-NAME-HERE}";
+        public const string HubConnectionString = "{HUB-CONNECTION-STRING-HERE}";
         public const string HubTagName = "default";
     }
 }
